44e925b538875418d6730d187be9d1cc003ca473
Tomas Roy <tomas.roy2019@gmail.com>
2019-08-21 16:06:44 +0900

    add git new logic,design,code

--- KinmuSample/Controllers/HomeController.cs
+++ KinmuSample/Controllers/HomeController.cs
@@ -3,16 +3,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using KinmuSample.Models;
 namespace KinmuSample.Controllers
 {
     public class HomeController : Controller
     {
+        KinmuSampleDataEntities _dbEntity = null;
         public ActionResult Index()
         {
+            //_dbEntity = new KinmuSampleDataEntities();
             if( Session["username"] == null){
                  return RedirectToAction("Login", "Account");
             }
+
+            ViewBag.Departments = new List<m_bumon>() {
+                    new m_bumon(){ id=1, bumon="IT" },
+                    new m_bumon(){ id=2, bumon="Operations" }                 
+            };
+
+            ViewBag.Subgroups = new List<m_bumon>() {
+                    new m_bumon(){ id=1, bumon="Developer" },
+                    new m_bumon(){ id=2, bumon="Newtork" }
+            };
             return View();
         }
 
---
+++ KinmuSample/Models/SearchViewModel.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KinmuSample.Models
+{
+    public class SearchViewModel
+    {
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public string StartDate { get; set; }
+
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy}")]
+        public string EndDate { get; set; }
+
+        [Display(Name="全社")]
+        public bool IsCompanyWideSelected { get; set; }
+
+        public bool IsDepartmentSelected { get; set; }
+
+        public string DepartmentCode { get; set; }
+
+        public string DepartmentName { get; set; }
+
+        public string WorkgroupCode { get; set; }
+
+        [Display(Name = "下位部門も含める")]
+        public bool IsIncludeSubDivision { get; set; }
+
+        [Display(Name = "事業所選択")]
+        public bool IsOfficeSelected { get; set; }
+
+        [Display(Name = "サブグループ選択")]
+        public bool IsSubgroupSelected { get; set; }
+
+        public bool IsIndividualSelected { get; set; }
+
+        [Display(Name = "出向者を除")]
+        public bool IsEmployeeExluded { get; set; }
+
+    }
+}
